c9e9fc723fb3d503673d10b1d96634926e660402
Ariya Nur Suparya <ariyaharyanto361@gmail.com>
2025-06-25 21:12:01 +0700

    mengubah tampilan utama

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,80 @@
+<nav class="pc-sidebar">
+  <div class="navbar-wrapper">
+    <div class="m-header flex items-center py-4 px-6 h-header-height">
+      <a href="../dashboard/index.html" class="b-brand flex items-center gap-3">
+        <!-- ========   Change your logo from here   ============ -->
+        <img src="../assets/images/logo-white.svg" class="img-fluid logo logo-lg" alt="logo" />
+        <img src="../assets/images/favicon.svg" class="img-fluid logo logo-sm" alt="logo" />
+      </a>
+    </div>
+    <div class="navbar-content h-[calc(100vh_-_74px)] py-2.5">
+      <ul class="pc-navbar">
+        <li class="pc-item pc-caption">
+          <label>Navigation</label>
+        </li>
+        <li class="pc-item">
+        <li class="pc-item">
+          <a href="../dashboard/index.html" class="pc-link">
+            <span class="pc-micon">
+              <i data-feather="home"></i>
+            </span>
+            <span class="pc-mtext">Dashboard</span>
+          </a>
+        </li>
+        <li class="pc-item pc-hasmenu">
+          <a href="../elements/bc_color.html" class="pc-link">
+            <span class="pc-micon"> <i data-feather="edit"></i></span>
+            <span class="pc-mtext">Kelola Users</span>
+          </a>
+        </li>
+        <li class="pc-item pc-hasmenu">
+          <a href="../elements/bc_typography.html" class="pc-link">
+            <span class="pc-micon"> <i data-feather="type"></i></span>
+            <span class="pc-mtext">Kelola Guru</span>
+          </a>
+        </li>
+        <li class="pc-item pc-hasmenu">
+          <a href="../elements/icon-feather.html" class="pc-link">
+            <span class="pc-micon"> <i data-feather="feather"></i></span>
+            <span class="pc-mtext">Kelola Kelas</span>
+          </a>
+        </li>
+        <li class="pc-item pc-hasmenu">
+          <a href="../elements/icon-feather.html" class="pc-link">
+            <span class="pc-micon"> <i data-feather="feather"></i></span>
+            <span class="pc-mtext">Kelola Jurusan</span>
+          </a>
+        </li>
+
+        <li class="pc-item pc-caption">
+          <label>Pages</label>
+          <i data-feather="monitor"></i>
+        </li>
+        <li class="pc-item pc-hasmenu">
+          <a href="../pages/login-v1.html" class="pc-link" target="_blank">
+            <span class="pc-micon"> <i data-feather="lock"></i></span>
+            <span class="pc-mtext">Login</span>
+          </a>
+        </li>
+        <li class="pc-item pc-hasmenu">
+          <a href="../pages/register-v1.html" class="pc-link" target="_blank">
+            <span class="pc-micon"> <i data-feather="user-plus"></i></span>
+            <span class="pc-mtext">Register</span>
+          </a>
+        </li>
+        <li class="pc-item pc-caption">
+          <label>Other</label>
+          <i data-feather="sidebar"></i>
+        </li>
+        <li class="pc-item">
+          <a href="../other/sample-page.html" class="pc-link">
+            <span class="pc-micon">
+              <i data-feather="sidebar"></i>
+            </span>
+            <span class="pc-mtext">Sample page</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
